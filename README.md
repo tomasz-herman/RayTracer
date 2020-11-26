e2b7b977713ae08bbbe96b9903b110a12b25c2c1
Tomasz Herman <tomasz.herman@protonmail.com>
2020-11-27 00:42:43 +0100

    Include libraries and build instructions

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # RayTracer
 Ray tracing implemented in c++
+
+# Building project
+- Create output directory for the build
+```
+mkdir out
+```
+- Change directory to newly created directory
+```
+cd out
+``` 
+- Generate `Makefile` using `Cmake`, replace `[Release|Debug]` with either `Release` or `Debug`
+```
+cmake -DCMAKE_BUILD_TYPE=[Release|Debug] ..
+```
+- Build project using `make`
+```
+make
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
-#include <iostream>
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/basic_file_sink.h>
+
+void setup_logger();
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    setup_logger();
     return 0;
+}
+
+void setup_logger() {
+    spdlog::set_default_logger(spdlog::basic_logger_mt("model_loader", "log/ray_tracer.log"));
+#ifdef DEBUG
+    spdlog::set_level(spdlog::level::trace);
+#else
+    spdlog::set_level(spdlog::level::info);
+#endif
+    spdlog::flush_on(spdlog::level::debug);
+    spdlog::flush_every(std::chrono::seconds(5));
+    spdlog::info("Logger set up and running. Log level is set to {}.", spdlog::level::to_string_view(spdlog::get_level()));
 }
